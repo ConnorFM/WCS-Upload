07ec31534c3a992cccbc9a81a2970c58189fe260
Foucauld Gaudin <47572692+ConnorFM@users.noreply.github.com>
2019-04-23 11:14:46 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title><?php echo $pageTitle ?></title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
-
-
-
-
 <!doctype html>
 <html lang="en">
   <head>
